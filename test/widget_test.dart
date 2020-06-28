ffa7c8c6fb6d4cf809919219ebfa2a8437f7e61a
shunjid <shunjid1862@gmail.com>
2020-06-28 21:05:29 +0600

    check for GitHub CI/CD

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,15 +1,12 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:donateplasma/providers/users.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:donateplasma/main.dart';
 
 void main() {
+  test('One Classroom Should Be Deleted, One Should Be Added', () {
+    final users = Users();
+    final initialLength = 10;
 
+    expect(users.userList.length, initialLength);
+  });
 }
